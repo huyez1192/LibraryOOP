27cbf99a2e282f19aa73d52d828b7dd6617bb8fb
Hương Giang <23021538@vnu.edu.vn>
2024-11-25 13:36:12 +0700

    home screen part 4

--- src/main/java/linkui/librarycontroller.java
+++ src/main/java/linkui/librarycontroller.java
@@ -1,5 +1,19 @@
 package linkui;
 
-public class librarycontroller {
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
+public class librarycontroller {
+    @FXML
+    private VBox overlay;
+
+    @FXML
+    private void showOverlay() {
+        overlay.setVisible(true);
+    }
+
+    @FXML
+    private void hideOverlay() {
+        overlay.setVisible(false);
+    }
 }
---
+++ src/main/java/linkui/searchcontroller.java
@@ -0,0 +1,21 @@
+package linkui;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class searchcontroller {
+    @FXML
+    private TextField search;
+
+    @FXML
+    private Button searchbutton;
+
+    @FXML
+    private Label searchlabel;
+
+    @FXML
+    public void initialize() {
+
+    }
+}
