2e8ee19b878f43555c5ee32dc16b2c72259cb554
linhdaynee <ntnlinh41@gmail.com>
2024-11-28 01:48:44 +0700

    Sửa lại Document

---
+++ src/main/java/Objects/Document.java
@@ -0,0 +1,85 @@
+package Objects;
+
+public class Document {
+    private String isbn;
+    private String title;
+    private String authors;
+    private String description;
+    private String categories;
+    private String thumbnailLink;
+    private String previewLink;
+    private boolean isBorrowed;
+
+    public Document(String isbn, String title, String authors, String description, String categories, String thumbnailLink, String previewLink) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authors = authors;
+        this.description = description;
+        this.categories = categories;
+        this.thumbnailLink = thumbnailLink;
+        this.previewLink = previewLink;
+        this.isBorrowed = false;
+    }
+
+    // Getters and setters
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return authors;
+    }
+
+    public void setAuthor(String authors) {
+        this.authors = authors;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public String getThumbnailLink() {
+        return thumbnailLink;
+    }
+
+    public void setThumbnailLink(String thumbnailLink) {
+        this.thumbnailLink = thumbnailLink;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrowBook() {
+        this.isBorrowed = true;
+    }
+
+    public void returnBook() {
+        this.isBorrowed = false;
+    }
+}
+
